bd7b752d1836cfc84d50dcb5a45d5cc8439037dd
Brady4080 <bradyp4080@gmail.com>
2026-08-27 07:45:10 -0500

    added some files to start working on fusion cal

---
+++ src/p5rfusioncal.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from 'react-router-dom'
+import './p5rfusioncal.css'
+
+import { rarePersonaeRoyal, rareCombosRoyal, arcana2CombosRoyal, specialCombosRoyal, dlcPersonaRoyal, inheritanceChartRoyal } from '../data/p5r/Data5Royal'
+import { itemMapRoyal } from '../data/p5r/ItemDataRoyal'
+import { personaMapRoyal } from '../data/p5r/PersonaDataRoyal'
+import { skillMapRoyal } from '../data/p5r/SkillDataRoyal'
+
+
